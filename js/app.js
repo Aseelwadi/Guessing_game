ed4b26ff9e44d1795d85404c3b53bb5d1c72dcb7
Aseelwadi <aseelalsqoor23@gmail.com>
2021-02-15 10:30:01 -0800

    about me and guessing game

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let userName = prompt('Enter your name please    :) ');
+alert('Welcome    '+userName +'    Are you ready to start game ?');
+
+let seasonGiss = prompt('Do you think Summer is my favorite season ?  answer Y-N / Yes-No')
+if(seasonGiss.toLowerCase() === 'yes' || seasonGiss.toUpperCase() === 'Y'){
+    //console.log('No :( ,,, My favorite season is Spring ^^ ')
+    alert ('No :(     ,,, My favorite season is Spring ^^ ');
+}
+else{
+    alert ('Thats right : )    ,,, My favorite season is Spring ^^ ');
+}
+
+
+
+let foodGuisse = prompt('Do you think I like Manssaf  ?  answer Y-N /Yes-No')
+if(foodGuisse.toLowerCase() === 'yes' || foodGuisse.toUpperCase() === 'Y'){
+    //console.log('No :( ,,,i dont like it  ^^ ')
+    alert ('No :(     ,,, i dont like it , I like Shawrmah  hhhhh ^_^ ');
+}
+else{
+    alert ('Thats right : )    ,,, i dont like it , you  guisse that   ^^ ');
+}
+
+
+let subjectGuisse = prompt('Do you think Maths is my favorite subject  ?  answer Y-N /Yes-No')
+if(subjectGuisse.toLowerCase() === 'yes' || subjectGuisse.toUpperCase() === 'Y'){
+    //console.log('No :( ,,, thats right   ^^ ')
+    alert ('Thats right : )      ,,,  i love maths so much  ^_^ ');
+}
+else{
+    alert ('No you did not guisse that  : (   ,,, i love it    ^^ ');
+}
+
+
+let personGuisse = prompt('Do you think My Mom is my favorit person   ?  answer Y-N /Yes-No')
+if(personGuisse.toLowerCase() === 'yes' || personGuisse.toUpperCase() === 'Y'){
+    //console.log('No :( ,,, thats right   ^^ ')
+    alert ('Thats right : )      ,,,  May God keep all mothers  ^_^ ');
+}
+else{
+    alert (' :(   Thats wrong   ,,, if it was not her , who do you think it would be ??   ^^ ');
+}
+
+let petGuisse = prompt('Do you think Cats is my favorit pet  ?  answer Y-N /Yes-No')
+if(petGuisse.toLowerCase() === 'yes' || petGuisse.toUpperCase() === 'Y'){
+    //console.log('No :( ,,, thats right   ^^ ')
+    alert ('Thats right : )      ,,,  I love cats sooo much  ^_^ ');
+}
+else{
+    alert ('  Thats Wrong :(     ,,,  i love cats    ^^ ');
+}
